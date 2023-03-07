f330375c6ed4aa70ff6ed733e5ee8bef1a51718c
cmasfo-github <cmasfo@gmail.com>
2023-03-07 16:01:32 +0900

    cpp modules

---
+++ _code/c-cpp/cpp/hello/src/cli.cpp
@@ -0,0 +1,8 @@
+
+import hello;
+
+int main() {
+  hello();
+
+  return 0;
+}
---
+++ _code/c-cpp/cpp/hello/src/hello.cpp
@@ -0,0 +1,11 @@
+
+module;
+
+#include "../inc/inc.hpp"
+
+export module hello;
+
+export void hello() {
+  cout << "Hello, C++!" << endl;
+  cout << "(successfully used C++20 modules system)" << endl;
+}
